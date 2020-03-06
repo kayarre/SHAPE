5b9de947d701b1179a973a917e15a2ff918590d3
Kurt R. Sansom <kurt.sansom@cfdrc.com>
2020-03-05 20:55:25 -0600

    attemping to get it working

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
-#include "shape.h"
-#include <QApplication>
 
-int main(int argc, char *argv[])
-{
-	QApplication a(argc, argv);
-	shape w;
-	w.show();
-	return a.exec();
+#include <QApplication>
+#include <QSurfaceFormat>
+#include <QVTKOpenGLNativeWidget.h>
+
+
+#include "shape.h"
+
+// int main(int argc, char *argv[])
+// {
+//   QApplication a(argc, argv);
+//   shape w;
+//   w.show();
+//   return a.exec();
+// }
+
+
+int main(int argc, char *argv[]) {
+  // before initializing QApplication, set the default surface format.
+  QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
+
+  QApplication app(argc, argv);
+
+  ShapeApp window;
+  window.show();
+
+  return app.exec();
 }
